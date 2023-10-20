584f52e6a2fec4755d88c0f792d73a9722479230
Antonio De Lucreziis <antonio.delucreziis@gmail.com>
2023-10-21 01:01:20 +0200

    fix: config typo

--- vite-ghpages.config.js
+++ vite-ghpages.config.js
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite'
 export default defineConfig({
     base: '/dm-calendar/',
     build: {
-        copyPublicDir: true,
         outDir: 'out/',
         emptyOutDir: true,
         minify: 'terser',

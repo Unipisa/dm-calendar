bcd566e29ad71843d63a5ceae9f09e02d1bb58e6
Antonio De Lucreziis <antonio.delucreziis@gmail.com>
2023-10-26 19:04:37 +0200

    fix

--- vite-phd.config.js
+++ vite-phd.config.js
@@ -1,35 +1,13 @@
-import { resolve } from 'path'
 
 import { defineConfig } from 'vite'
 
-import rollupPluginSizes from 'rollup-plugin-sizes'
-
-export default defineConfig(configEnv => {
-    return {
-        server: {
-            port: 3000,
-        },
-        build: {
-            outDir: 'out/phd/',
-            emptyOutDir: true,
-            minify: 'terser',
-            lib: {
-                formats: ['iife'],
-                entry: resolve(__dirname, 'src/dm-calendar-phd.js'),
-                name: 'DMCalendar',
-                fileName: 'dm-calendar',
-            },
-            rollupOptions: {
-                output: {
-                    assetFileNames: 'dm-calendar.[ext]',
-                },
-            },
-        },
-        plugins:
-            configEnv.command === 'build'
-                ? // mostro alcune metriche sul bundle generato
-                  [rollupPluginSizes()]
-                : // altrimenti non usiamo nessun plugin per ora
-                  [],
-    }
+export default defineConfig({
+    build: {
+        outDir: 'out/phd/',
+        emptyOutDir: true,
+        minify: 'terser',
+        rollupOptions: {
+            input: 'phd.html',
+        }
+    },
 })
